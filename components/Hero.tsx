c3ce161d1369de3296b10040f01807e7048d4c58
Prakhar Singh <prakhar22153054@akgec.ac.in>
2024-06-23 11:55:54 +0530

    socail component added

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
-import ParticlesComponent from './Particle';
+import ParticlesComponent from './particle';
 import TextEffect from './TextEffect';
+import SocialButtons from './social';
+import { ArrowDownTrayIcon } from '@heroicons/react/20/solid';
 
 const Hero = () => {
   return (
@@ -16,21 +18,28 @@ const Hero = () => {
 
       {/* Main content */}
       <div className="absolute top-0 left-0 w-full h-full flex items-center justify-center">
-        <div className="w-[80%] grid grid-cols-1 lg:grid-cols-2 gap-12 text-center text-white">
+        <div className="w-[90%] lg:w-[80%] grid grid-cols-1 lg:grid-cols-2 gap-12 text-center text-white">
           
           {/* Text content */}
-          <div className="flex flex-col items-center justify-center lg:items-start">
+          <div className="flex flex-col items-center lg:items-start lg:pr-12">
             <h1 className="text-4xl md:text-6xl font-bold mb-4">
               HI, I'M <span className="text-yellow-400">PRAKHAR!</span>
             </h1>
             <TextEffect />
-            <p className="text-lg md:text-xl text-gray-300 mb-8">
+            <p className="mt-6 text-lg text-[#ffffff92] text-center lg:text-left">
               Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ipsum sapiente voluptates ducimus laboriosam aliquam? Fuga similique asperiores ea dolorem id.
             </p>
+            <div className="mt-8 flex flex-col sm:flex-row items-center space-y-6 sm:space-y-0 sm:space-x-6">
+              <button className="px-8 py-4 text-lg font-bold uppercase bg-[#55e6a5] text-black hover:bg-yellow-400 transition duration-200 flex items-center space-x-2">
+                <p>Download Resume</p>
+                <ArrowDownTrayIcon className="w-6 h-6 text-black" />
+              </button>
+            </div>
+            <SocialButtons />
           </div>
 
           {/* Image */}
-          <div className="hidden lg:flex items-center justify-center w-[500px] h-[500px] rounded-full overflow-hidden">
+          <div className="hidden lg:flex items-center justify-center lg:justify-start lg:ml-14 w-[500px] h-[500px] rounded-full overflow-hidden">
             <img
               src="/images/u1.jpg"
               alt="user"
---
+++ components/social.tsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import { FaGithub, FaLinkedin, FaInstagram } from 'react-icons/fa';
+
+const socialbuttons = () => {
+  return (
+    <div className="flex space-x-4 mt-6">
+      {/* GitHub */}
+      <a
+        href="https://github.com/prakharsingh-74"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="p-3 bg-gray-800 rounded-full hover:bg-gray-600 transition duration-300"
+      >
+        <FaGithub className="text-white w-6 h-6" />
+      </a>
+
+      {/* LinkedIn */}
+      <a
+        href="https://www.linkedin.com/in/prakharsingh74/"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="p-3 bg-blue-600 rounded-full hover:bg-blue-400 transition duration-300"
+      >
+        <FaLinkedin className="text-white w-6 h-6" />
+      </a>
+
+      {/* Instagram */}
+      <a
+        href="https://www.instagram.com/prakhar.singh74"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="p-3 bg-pink-500 rounded-full hover:bg-pink-400 transition duration-300"
+      >
+        <FaInstagram className="text-white w-6 h-6" />
+      </a>
+    </div>
+  );
+};
+
+export default socialbuttons;
